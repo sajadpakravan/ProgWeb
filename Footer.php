21e004c322996fc4ea803bbf8288357fe5039d76
Sajad Pakravan <79419782+sajadpakravan@users.noreply.github.com>
2021-03-13 18:15:06 +0330

    Add files via upload

---
+++ Footer.php
@@ -0,0 +1,86 @@
+<footer>
+	
+	<div class="footer_div">
+		<p class="Title_Footer">اطلاعات تماس</p>
+		
+		<p class="Cont_Footer">
+			<img src="http://localhost/WebP/Images/Icon/LocationIcon.png"/>آدرس: اصفهان - نجف آباد
+		</p>
+		
+		<p class="Cont_Footer">
+			<img src="http://localhost/WebP/Images/Icon/PhoneIcon.png"/>شماره تماس: 09380521118 و 09123456789
+		</p>
+		
+		<p class="Cont_Footer">
+			<img src="http://localhost/WebP/Images/Icon/EmailIcon.png"/> ایمیل: pakravan1118sa@gmail.com
+		</p>
+		
+		<a href="#"><p class="Cont_Footer instaFooter StyleLink">
+			<img src="http://localhost/WebP/Images/Icon/InstagramIcon.png" class="iconITT">انستاگرام
+			</p></a>
+		
+		<a href="#"><p class="Cont_Footer telegFooter StyleLink">
+			<img src="http://localhost/WebP/Images/Icon/TelegramIcon.png" class="iconITT">تلگرام
+			</p></a>
+		
+		<a href="#"><p class="Cont_Footer twittFooter StyleLink">
+			<img src="http://localhost/WebP/Images/Icon/TwitterIcon.png" class="iconITT">توییتر
+			</p></a>
+	</div>
+	
+	<div class="footer_div">
+		<p class="Title_Footer">دسته‌بندی محصولات</p>
+		
+		<a href="#"><p class="Cont_Footer StyleLink">
+			<img src="http://localhost/WebP/Images/Icon/KitchenIcon.png"/>لوازم آشپزخانه
+			</p></a>
+		
+		<a href="#"><p class="Cont_Footer StyleLink">
+			<img src="http://localhost/WebP/Images/Icon/SofaIcon.png"/>مبلمان
+			</p></a>
+		
+		<a href="#"><p class="Cont_Footer StyleLink">
+			<img src="http://localhost/WebP/Images/Icon/CoHeIcon.png"/>سرمایش و گرمایش
+			</p></a>
+		
+		<a href="#"><p class="Cont_Footer StyleLink">
+			<img src="http://localhost/WebP/Images/Icon/TVIcon.png"/>صوتی و تصویری
+			</p></a>
+		
+		<a href="#"><p class="Cont_Footer StyleLink">
+			<img src="http://localhost/WebP/Images/Icon/WashingIcon.png"/>لوازم شستشو
+			</p></a>
+	</div>
+	
+	<div class="footer_div">
+		<p class="Title_Footer">صفحه‌های سایت</p>
+		
+		<a href="index.php"><p class="Cont_Footer StyleLink">صفحه اصلی</p></a>
+		
+		<a href="AboutUs.php"><p class="Cont_Footer StyleLink">درباره ما</p></a>
+		
+		<a href="#"><p class="Cont_Footer StyleLink">برند ها</p></a>
+		
+		<a href="#"><p class="Cont_Footer StyleLink">وبلاگ</p></a>
+		
+		<a href="ContactUs.php"><p class="Cont_Footer StyleLink">تماس با ما</p></a>
+	</div>
+	
+	<div>
+		<p class="Title_Footer">آخرین مقالات</p>
+<!--
+		<p class="Cont_Footer">آدرس: اصفهان - نجف آباد</p>
+		<p class="Cont_Footer">شماره تماس: 09380521118 و 09123456789</p>
+		<p class="Cont_Footer instaFooter"><img src="Images/Icon/InstagramIcon.png" class="iconITT">انستاگرام</p>
+		<p class="Cont_Footer telegFooter"><img src="Images/Icon/TelegramIcon.png" class="iconITT">تلگرام</p>
+		<p class="Cont_Footer twittFooter"><img src="Images/Icon/TwitterIcon.png" class="iconITT">توییتر</p>
+-->
+	</div>
+	
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ Settings.php
@@ -0,0 +1,20 @@
+<?php
+
+if(!defined('DBHOST') )
+	define('DBHOST', 'localhost');
+
+if(!defined('DBUSER') )
+	define('DBUSER', 'root');
+
+if(!defined('DBPASS'))
+	define('DBPASS', '');
+
+if(!defined('DBNAME'))
+	define('DBNAME', 'home_appliances');
+
+$dbHost = DBHOST;
+$dbUser = DBUSER;
+$dbPass = DBPASS;
+$dbName = DBNAME;
+
+?>
