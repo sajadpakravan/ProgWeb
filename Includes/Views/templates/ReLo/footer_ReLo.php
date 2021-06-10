874d45499759289eda8d7f82abe5fb44d5bc8fc6
Sajad Pakravan <79419782+sajadpakravan@users.noreply.github.com>
2021-06-10 05:46:31 +0430

    Delete Includes directory

--- Includes/Views/templates/ReLo/footer_ReLo.php
+++
@@ -1,4 +0,0 @@
-<div class="linkMP">
-			<a href="<?php echo($SITE_URL) ?>">بازگشت به صفحه اصلی</a>
-		</div>
-	</div>
--- Includes/Views/templates/ReLo/header_ReLo.php
+++
@@ -1,4 +0,0 @@
-<body>
-	
-	<div class="ReLoCover">
-		<div><img src="<?php echo Assets("Images/Icon/SiteIcon.png"); ?>"/></div>
